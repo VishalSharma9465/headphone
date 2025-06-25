e1c50afc743b3ef779d87ae23a702d0ccd7a33bc
VishalSharma9465 <sabisharma855@gmail.com>
2025-06-25 16:55:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ $db = "sys";
 $conn = mysqli_connect($servername, $username, $password,$bd);
 
   if(isset($_GET["send"])){
-  $fanme = $_GET["fname"];
+  $fname = $_GET["fname"];
   $email = $_GET["email"];
   $message = $_GET["message"];
 
   $sqlir = "INSERT INTO headcore (n_name,e_email,message) VALUES ('$fname','$email','$message')";
-if(mysqli_quary($conn,$sqlir)){
-  echo data store;
-}
+mysqli_quary($conn,$sqlir))
+
+
   }
   ?>
