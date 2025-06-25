2061bc309671cc1fa912219ec08f6fd9034a51a8
VishalSharma9465 <sabisharma855@gmail.com>
2025-06-25 16:43:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $password = "Vishal@123";
 $db = "sys";
 $conn = mysqli_connect($servername, $username, $password,$bd);
 
-  if(isset($_POST["send"])){
-  $fanme = $_POST["fname"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+  if(isset($_GET["send"])){
+  $fanme = $_GET["fname"];
+  $email = $_GET["email"];
+  $message = $_GET["message"];
 
   $sqlir = "INSERT INTO headcore (n_name,e_email,message) VALUES ('$fname','$email','$message')";
 mysqli_quary($conn,$sqlir);
