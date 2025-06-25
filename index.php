90373360048e15ecb14aab90fe28fc9464065a36
VishalSharma9465 <sabisharma855@gmail.com>
2025-06-25 16:52:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ $conn = mysqli_connect($servername, $username, $password,$bd);
   $message = $_GET["message"];
 
   $sqlir = "INSERT INTO headcore (n_name,e_email,message) VALUES ('$fname','$email','$message')";
-mysqli_quary($conn,$sqlir);
+if(mysqli_quary($conn,$sqlir)){
+  echo data store;
+}
   }
   ?>
